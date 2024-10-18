48a05da6333ca4737eee531ebab192924e475a2c
pkj <pkjifinity@outlook.com>
2024-10-18 12:38:20 +0800

    [bugfix]:cmake-lint Changing regular expressions to match more information

--- lua/null-ls/builtins/diagnostics/cmake_lint.lua
+++ lua/null-ls/builtins/diagnostics/cmake_lint.lua
@@ -1,6 +1,17 @@
 local h = require("null-ls.helpers")
 local methods = require("null-ls.methods")
 
+local get_severity = function(severity)
+    if severity == "E" then
+        return h.diagnostics.severities.error
+    elseif severity == "W" then
+        return h.diagnostics.severities.warning
+    elseif severity == "C" or severity == "R" or severity == "I" then
+        return h.diagnostics.severities.information
+    end
+    return h.diagnostics.severities.warning
+end
+
 return h.make_builtin({
     name = "cmake_lint",
     meta = {
@@ -17,20 +28,24 @@ return h.make_builtin({
         format = "line",
         to_stdin = false,
         from_stderr = true,
-        on_output = h.diagnostics.from_pattern(
-            [[(%d+),(%d+): %[((%w)[%d]+)%] (.+)]],
-            { "row", "col", "code", "severity", "message" },
-            {
-                severities = {
-                    E = h.diagnostics.severities["error"],
-                    W = h.diagnostics.severities["warning"],
-                    C = h.diagnostics.severities["information"],
-                    R = h.diagnostics.severities["information"],
-                    I = h.diagnostics.severities["information"],
-                },
-                offsets = { col = 1 },
+        on_output = function(line, params)
+            if not vim.startswith(line, params.bufname) or line == params.bufname then
+                return
+            end
+            local row, col, code, sev, msg = string.match(line, "(%d+),?([0-9]*): %[((%w)[%d]+)%] (.+)")
+
+            if col == "" then
+                col = "1"
+            end
+            local ret = {
+                row = row,
+                col = col,
+                message = msg,
+                code = code,
+                severity = get_severity(sev),
             }
-        ),
+            return ret
+        end,
     },
     factory = h.generator_factory,
 })
